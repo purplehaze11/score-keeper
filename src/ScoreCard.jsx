3d5dd788fda4ca686c48ba4768d6f77c1f56e475
Tio Rambe Putra <tiorambe1104@gmail.com>
2023-07-18 04:22:42 +0700

    Add target function to limit increment function

--- src/ScoreCard.jsx
+++ src/ScoreCard.jsx
@@ -3,12 +3,13 @@ import { v4 as uuidv4 } from 'uuid';
 
 const generateArr = (num) => Array(num).fill(0);
 
-function ScoreCard({ numPlayers = 2 }) {
+function ScoreCard({ numPlayers = 2, target = 3 }) {
 	const [card, setCard] = useState(generateArr(numPlayers));
+
 	const increment = (index) => {
 		setCard((oldCard) => {
 			return [...oldCard].map((i, ind) => {
-				if (index === ind) {
+				if (index === ind && i !== target) {
 					return i + 1;
 				} else {
 					return i;
@@ -16,6 +17,7 @@ function ScoreCard({ numPlayers = 2 }) {
 			});
 		});
 	};
+
 	const reset = () => {
 		setCard((prevCard) => {
 			return [...prevCard].map((i) => {
@@ -24,12 +26,19 @@ function ScoreCard({ numPlayers = 2 }) {
 		});
 	};
 
-	const cardUnit = card.map((unit, id) => (
+	const isTarget = (n) => n === target;
+
+	const cardUnit = card.map((unit, id, arr) => (
 		<li key={uuidv4()}>
 			<p id={id}>
 				Player{id + 1}: {unit}
 			</p>
-			<button onClick={() => increment(id)}>+1</button>
+			{/* <button onClick={() => increment(id)}>+1</button> */}
+			{arr.some(isTarget) ? (
+				<button disabled>+1</button>
+			) : (
+				<button onClick={() => increment(id)}>+1</button>
+			)}
 		</li>
 	));
 	return (
