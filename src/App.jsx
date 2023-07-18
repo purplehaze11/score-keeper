32e2feefb7fdc5082c515f07f9d21995fc45d2c4
Tio Rambe Putra <tiorambe1104@gmail.com>
2023-07-18 16:59:58 +0700

    Create winning count select element

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,20 @@ import './App.css';
 
 function App() {
 	return (
-		<div className='card w-96 bg-base-100 shadow-xl'>
+		<div className='card min-w-md max-w-lg bg-base-200 shadow-xl m-auto'>
 			<figure>
 				<img
+					className='h-64 w-fit'
 					src='https://images.unsplash.com/photo-1534158914592-062992fbe900?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3784&q=80'
 					alt='Tennis Table'
 				/>
 			</figure>
 			<div className='card-body'>
-				<h2 className='card-title'>Multi Player Game Score Keeper</h2>
-				<p>If a dog chews shoes whose shoes does he choose?</p>
-				<div className='card-actions justify-center'>
-					<ScoreCard numPlayers={4} />
-				</div>
+				<h2 className='card-title'>Multiplayer Game Score Keeper</h2>
+				<ScoreCard
+					numPlayers={4}
+					target={10}
+				/>
 			</div>
 		</div>
 	);
--- src/ScoreCard.jsx
+++ src/ScoreCard.jsx
@@ -30,21 +30,51 @@ function ScoreCard({ numPlayers = 2, target = 3 }) {
 
 	const cardUnit = card.map((unit, id, arr) => (
 		<li key={uuidv4()}>
-			<p id={id}>
-				Player{id + 1}: {unit}
-			</p>
-			{/* <button onClick={() => increment(id)}>+1</button> */}
-			{arr.some(isTarget) ? (
-				<button disabled>+1</button>
-			) : (
-				<button onClick={() => increment(id)}>+1</button>
-			)}
+			<div className='flex flex-col'>
+				<p
+					className='m-2'
+					id={id}
+				>
+					Player{id + 1}: {unit}
+				</p>
+				{arr.some(isTarget) ? (
+					<button
+						className='btn btn-disabled'
+						disabled
+					>
+						+1
+					</button>
+				) : (
+					<button10
+						className='btn btn-outline btn-primary'
+						onClick={() => increment(id)}
+					>
+						+1
+					</button10>
+				)}
+			</div>
 		</li>
 	));
+
 	return (
 		<>
-			<ul>{cardUnit}</ul>
-			<button onClick={reset}>Reset</button>
+			<div className='card h-10 w-auto bg-neutral text-neutral-content flex'>
+				{card.map((unit, id) =>
+					unit === target ? (
+						<p className='text-green-400 py-2'>Player{id + 1} win!</p>
+					) : null
+				)}
+			</div>
+
+			<div>
+				<ul className='flex flex-row flex-wrap justify-evenly'>{cardUnit}</ul>
+			</div>
+			<button
+				className='btn btn-outline btn-error mt-4'
+				onClick={reset}
+			>
+				Reset
+			</button>
 		</>
 	);
 }
